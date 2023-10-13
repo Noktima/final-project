af001aeb0f9957f586039264fdd56cad3b987a27
Noktima <aleksej.konovalov.2000@gmail.com>
2023-10-13 16:38:00 +0700

    Сделал ТестАПИ

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import TestApi from './components/TestApi/TestApi';
 
 function App() {
   return (
     <div className="App">
+
 
     </div>
   );
--- src/components/TestApi/TestApi.jsx
+++ src/components/TestApi/TestApi.jsx
@@ -1,20 +1,8 @@
-import axios from 'axios';
-
-
-const axios = require('axios');
-
-const options = {
-  method: 'GET',
-  url: 'https://api-basketball.p.rapidapi.com/timezone',
-  headers: {
-    'X-RapidAPI-Key': '4ff47b654amsh6c39eac0138d830p1a2b02jsnfe73c009c89b',
-    'X-RapidAPI-Host': 'api-basketball.p.rapidapi.com'
-  }
-};
-
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
+fetch('https://api.publicapis.org/entries', {
+})
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
